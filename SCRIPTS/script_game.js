4cb414b23842aafc2117a91cc519563492ca6f7d
TimRamminger <137364081+TimRamminger@users.noreply.github.com>
2023-10-04 16:49:23 +0200

    mainSite players fixes + mainGame footer height fix

--- SCRIPTS/script_game.js
+++ SCRIPTS/script_game.js
@@ -28,7 +28,80 @@ function loadJson() {
 }
 
 function onLoad() {
-    var data = loadJson();
+    var data = {
+        "tasksNoPlayer":[
+          {
+              "task":"Jeder der heute Abend schon gekotzt hat, muss trinken",
+              "toDrink":1
+          },
+          {
+            "task":"Jeder der schonmal im Zug betrunken eingeschlafen ist muss § Schlücke trinken",
+            "toDrink":3
+          },
+          {
+            "task":"Jeder der mehr als zwei Sprachen sprechen kann, muss trinken",
+            "toDrink":1
+          },
+          {
+            "task":"Jeder der nicht Cedric Emanuel Weiss heißt, muss trinken",
+            "toDrink":1
+          },
+          {
+            "task":"Jeder der schonmal seine Schuhe verloren hat muss § Schlücke trinken",
+            "toDrink":3
+          },
+          {
+            "task":"Jeder der schonmal im Zug betrunken eingeschlafen ist muss § Schlücke trinken",
+            "toDrink":3
+          },
+          {
+            "task":"Jüngster und Ältester Spieler trinkt",
+            "toDrink":1
+          },
+          {
+            "task":"Jeder der nicht an einer Uni ist, muss exen",
+            "toDrink":10
+          },
+          {
+            "task":"Jeder der schonmal im Zug betrunken eingeschlafen ist muss $ Schlücke trinken",
+            "toDrink":3
+          }
+        ],
+        "tasks1Player": [
+          {
+            "task":", du darfst eine neue Regel einführen",
+            "toDrink":0
+          },
+          {
+            "task":", deine Mitspieler haben 30 Sekunden, um dich zum Lachen zu bringen. Wenn sie es schaffen trinkst du, ansonsten trinken sie",
+            "toDrink":1
+          },
+          {
+            "task":"erzähl einen Witz, wenn keiner lacht trink",
+            "toDrink":1
+          },
+          {
+            "task":"trinkt ein Shot",
+            "toDrink":1
+          },
+          {
+            "task":"muss einen Trinkbuddy bestimmen",
+            "toDrink":0
+          },
+          {
+            "task":"muss auf ex trinken",
+            "toDrink":10
+          },
+          {
+            "task":"trinkt, einfach so weil er/sie so toll ist",
+            "toDrink":1
+          },
+          {
+            "task":"bestimmt eine Person die trinken muss",
+            "toDrink":1
+          }
+        ]
+      };
     console.log(data);
     tasksNoPlayer = data["tasksNoPlayer"];
     tasks1Player = data["tasks1Player"];
@@ -132,8 +205,9 @@ function removeRadioButtons(element) {
 }
 
 function onClick(element) {
-    console.log(mouseY);
-    if(mouseY>645) return;
+    var footerHeight = document.getElementById("footer").offsetHeight;
+    
+    if(mouseY>window.innerHeight-footerHeight-5) return;
     if(mouseX<180) {
         prevTask(element);
     }
--- SCRIPTS/script_home.js
+++ SCRIPTS/script_home.js
@@ -19,6 +19,10 @@ function addPlayerOnEnter(element) {
 }
 
 function addPlayer() {
+    if(document.getElementById("input").value.length>22) {
+        alert("Name zu lang!");
+        return;
+    }
     if(!document.getElementById("input").value =="") {
         players.push(document.getElementById("input").value);
   
@@ -30,9 +34,9 @@ function addPlayer() {
     }
 }
 
-function removePlayer(btn) {
-    players.splice(players.indexOf(btn.previousSibling.innerHTML),1);
-    btn.parentNode.remove();
+function removePlayer(txt) {
+    players.splice(players.indexOf(txt.innerHTML),1);
+    txt.remove();
     console.log(players);
     saveData();
 
@@ -46,35 +50,14 @@ function test() {
 
 function showElements(i) {
     for(;i<players.length;i++) {
-        var div = document.createElement("div");
-        var h3 = document.createElement("h3");
-        var button = document.createElement("button");
+        var p = document.createElement("p");
 
-        div.style.display = "flex";
-        div.style.margin = "5px"; 
-        div.style.width = "max-content";   
-
-        h3.style.color = "white";
-        h3.style.margin = "0px";
-        h3.style.fontSize = "35px";
-        
-        button.style.marginLeft = "10px";
-        button.style.marginTop = "5px";
-        button.innerHTML = "X";
-        button.style.height = "35px";
-        button.style.width = "35px";
-
-        button.style.border = "3px solid rgba(255, 255, 255, 0.774)";
-        button.style.borderRadius = "15px";
-        button.style.backgroundColor = "rgba(173, 170, 170, 0.644)";
-        button.setAttribute("onclick", "removePlayer(this)");
+        p.setAttribute("onclick", "removePlayer(this)");
 
         var text = document.createTextNode(players[i]);
-        h3.appendChild(text);
+        p.appendChild(text);
 
-        div.appendChild(h3);
-        div.appendChild(button);
-        document.getElementById("player").appendChild(div);
+        document.getElementById("player").appendChild(p);
     }
 }
 
